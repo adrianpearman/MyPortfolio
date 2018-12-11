5c482562ff6ac08f1b4946a479a37bf2533a9723
Adrian Pearman <adrianpearman12@gmail.com>
2018-12-10 20:48:01 -0500

    updating client side build file

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30f2dca4d8d7a77fcb27bb47a48b716d.js"
+  "/precache-manifest.7cab24786d16f036117dc5fe6d90e5bf.js"
 );
 
 workbox.clientsClaim();
