c3f0f2103932ddc9f57e716a6ab0a6054c8b27fc
Adrian Pearman <adrianpearman12@gmail.com>
2020-01-23 03:48:26 -0500

    updating primary color for portfolio.

--- client/src/components/experience-items.js
+++ client/src/components/experience-items.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import { VerticalTimelineElement } from 'react-vertical-timeline-component'
+import React from "react";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
 
-const ExperienceItems = (props) => {
+const ExperienceItems = props => {
   return (
     <VerticalTimelineElement
-        className="vertical-timeline-element--work experience-title"
-        iconStyle={{ background: '#a8ce00' }}
+      className="vertical-timeline-element--work experience-title"
+      iconStyle={{ background: "#5acacb" }}
     >
-        <h5 className="vertical-timeline-element-title mb-3 experience-title">
-            <span className='emphasize-title'>Company:</span> {props.employer} 
-        </h5>
-        <h5 className="vertical-timeline-element-title mb-3 experience-title">
-            <span className='emphasize-title'>Title:</span> {props.title} 
-        </h5>
-        <h5 className="vertical-timeline-element-subtitle mb-3 experience-title">
-            <span className='emphasize-title'>Location:</span> {props.location}
-        </h5>
-        <h5 className="vertical-timeline-element-subtitle mb-3 experience-title">
-            <span className='emphasize-title'>Dates:</span> {props.dates}
-        </h5>
-        <h5 className='experience-title experience-title-responsibility'>
-            <span className='emphasize-title'>Responsibilities:</span>
-        </h5>
-        <ul className='experience-list'>
-            {
-                props.responsibilities.map((responsibility, index) => {
-                    return <li key={index}>{responsibility}</li>
-                })
-            }
-        </ul>
+      <h5 className="vertical-timeline-element-title mb-3 experience-title">
+        <span className="emphasize-title">Company:</span> {props.employer}
+      </h5>
+      <h5 className="vertical-timeline-element-title mb-3 experience-title">
+        <span className="emphasize-title">Title:</span> {props.title}
+      </h5>
+      <h5 className="vertical-timeline-element-subtitle mb-3 experience-title">
+        <span className="emphasize-title">Location:</span> {props.location}
+      </h5>
+      <h5 className="vertical-timeline-element-subtitle mb-3 experience-title">
+        <span className="emphasize-title">Dates:</span> {props.dates}
+      </h5>
+      <h5 className="experience-title experience-title-responsibility">
+        <span className="emphasize-title">Responsibilities:</span>
+      </h5>
+      <ul className="experience-list">
+        {props.responsibilities.map((responsibility, index) => {
+          return <li key={index}>{responsibility}</li>;
+        })}
+      </ul>
     </VerticalTimelineElement>
-  )
-}
+  );
+};
 
-export default ExperienceItems
+export default ExperienceItems;
