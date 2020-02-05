0cf28fb33ebea0edd088153af8b0c935aa5a7433
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-05 12:43:00 -0500

    update

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,6 @@
 module.exports = {
-  mLabURI:
-    "mongodb://apsp_1:REDbull45!@ds061741.mlab.com:61741/apsp_portfolio-dev",
+  mLabURI: "",
   mLabCollection: "apsp_portfolio-dev",
   myEmail: "adrianpearman12@gmail.com",
-  sendGridAPIKey:
-    "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"
+  sendGridAPIKey: ""
 };
