e8d38628edbad5300d12fd3822b702ce60d59de0
Adrian Pearman <adrianpearman12@gmail.com>
2020-12-13 20:40:24 -0500

    updated server.js to use new keys. slight refactoring to file preformed

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const ngrok = require("ngrok");
+// const ngrok = require("ngrok");
 const sslRedirect = require("heroku-ssl-redirect");
 // Environment Variables
 const app = express();
@@ -15,9 +15,6 @@ const keys = require("./config/keys");
 require("./models/Contact");
 require("./models/Experience");
 require("./models/Projects");
-// dB Connection
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mLabURI, { useNewUrlParser: true });
 // Express Middleware
 app.use(sslRedirect());
 app.use(cors());
@@ -26,21 +23,25 @@ app.use(bodyParser.json());
 // Routes
 const apiRoutes = require("./routes/apiRoutes");
 app.use("/api", apiRoutes);
-
+// dB Connection
+mongoose.Promise = global.Promise;
+// Environment Variables
 if (process.env.NODE_ENV === "production") {
+  mongoose.connect(keys.mongoDBURI_PROD, { useNewUrlParser: true });
   app.use(express.static(path.join(__dirname, "../client", "build")));
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 } else {
+  mongoose.connect(keys.mongoDBURI_DEV, { useNewUrlParser: true });
   //ngrok
   // (async function() {
   //   const url = await ngrok.connect(PORT);
   //   console.log(url);
   // })();
 }
-
 // Server Startup
 app.listen(PORT, () => {
+  console.clear();
   console.log(`Server running on PORT: ${PORT}`);
 });
