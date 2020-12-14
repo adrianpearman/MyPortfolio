f12677617e153b3b8fd043141938624da1bd8f6a
Adrian Pearman <adrianpearman12@gmail.com>
2020-12-13 20:38:40 -0500

    update to the Projects model

--- backend/models/Projects.js
+++ backend/models/Projects.js
@@ -3,10 +3,11 @@ const { Schema } = mongoose;
 
 const ProjectsSchema = new Schema({
   title: { type: String },
-  overview: [{ type: String }],
+  overview: { type: String },
   liveLink: { type: String },
   repoLink: { type: String },
-  image: { type: String }
+  image: { type: String },
+  stack: [String],
 });
 
-mongoose.model("projects", ProjectsSchema);
+mongoose.model("project", ProjectsSchema);
