dc49f7f0bae89920d6d1e24931bc6ba95606925b
Adrian Pearman <adrianpearman12@gmail.com>
2020-12-13 20:38:00 -0500

    updated config file to hold new mongoclient information

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -3,4 +3,6 @@ module.exports = {
   mLabCollection: process.env.mLabCollection,
   myEmail: process.env.EMAIL,
   sendGridAPIKey: process.env.sendGridAPIKey,
+  mongoDBURI_DEV: process.env.mongoDBURI_DEV,
+  mongoDBURI_PROD: process.env.mongoDBURI_PROD,
 };
