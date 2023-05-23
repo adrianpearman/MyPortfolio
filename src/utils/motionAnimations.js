49ddf96856d3080a4041bb825ef6e99de8a9cc00
Adrian Pearman <adrianpearman12@gmail.com>
2023-05-23 16:54:15 -0400

    created util folder for functions

---
+++ src/utils/motionAnimations.js
@@ -0,0 +1,41 @@
+export const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "string",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+export const dropOver = {
+  hidden: {
+    x: "-100vw",
+    opacity: 0,
+  },
+  visible: {
+    x: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      type: "string",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    opacity: 0,
+  },
+};
